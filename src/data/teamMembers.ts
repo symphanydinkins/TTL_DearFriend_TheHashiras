047b5dd88f9495386944d6c4e31ebe29e4ef929e
Symphany Dinkins <symphanydinkinss@gmail.com>
2026-07-15 23:30:05 -0400

    Updated teamMembers.ts

--- src/data/teamMembers.ts
+++ src/data/teamMembers.ts
@@ -34,7 +34,7 @@ export const TEAM_MEMBERS: TeamMember[] = [
     skills: ['Cybersecurity', 'Cloud Security', 'HTML & CSS', 'The Knowledge House (TKH)'],
     interests: ['Electric Guitar', 'Flute', 'Sports'],
     education: ['Upcoming Senior in High School', 'Atlanta Technical College (Fall 2026)'],
-     cardColor: '#f9edff',
+     cardColor: '#f9edf',
     icon: Code,
     social: {
       github: 'https://github.com',
